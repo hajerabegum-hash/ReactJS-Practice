dfd47ba1e43d3f90a5df3dcc8e26d34add538dee
hajerabegum-hash/css <hajerabegum20022@gmail.com>
2026-09-23 16:39:52 +0530

    REACT-PRACTICE

---
+++ README.md
@@ -0,0 +1,26 @@
+# steps to create a react application
+
+-> create a folder (Batch-6-React)
+
+-> create index.html and script.js
+
+-> add boilerplate to index.html and script with src="script.js"
+
+-> npm init (Batch-6-React)
+
+-> npm i react react-dom
+
+-> npm i parcel
+
+In script.js file on top
+import React from "react"
+
+-> import ReactDOM from "react-dom/client"
+
+-> in package.json change to "type": "module"
+
+-> in script tag add attribute type: "module"
+
+->  npx parcel index.html
+
+-> npx parcel build index.html
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Method-2 (VANNILLA SCRIPT)
 
 const h2 = document.createElement("h2");
 h2.innerText = "I 'M H2 FROM JAVASCRIPT";
-document.querySelector("body").insertAdjacentElement("afterbegin", h2);
+const parent = React.createElement("div", { id: "div1-react" }, [
+  React.createElement("h1", {}, "HI! Iam H1 from ReactJS"),
+  React.createElement("h2", {}, "HI! Im H2 FROM REACT JS"),
+  React.createElement("div", { id: "div-child" }, [
+    React.createElement("h1", {}, "HI! IM H1 FROM CHILD-DIV "),
+    React.createElement("h2", {}, "HI! IM H2 FROM CHILD-DIV "),
+  ]),
+]);
+
+let root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(parent);
+// document.querySelector("body").insertAdjacentElement("afterbegin", h2);
 
 //TARGET TO MAKE THIS STRUCTURE USING REACT JS
 // <div>
@@ -15,11 +29,3 @@ document.querySelector("body").insertAdjacentElement("afterbegin", h2);
 // </div>
 
 //METHOD-3
-const parent = React.createElement("div", { id: "div1-react" }, [
-  React.createElement("h1", {}, "HI! Iam H1 from ReactJS"),
-  React.createElement("h2", {}, "HI! Im H2 FROM REACT JS"),
-  React.createElement("div", { id: "div-child" }, [
-    React.createElement("h1", {}, "HI! IM H1 FROM CHILD-DIV "),
-    React.createElement("h2", {}, "HI! IM H2 FROM CHILD-DIV "),
-  ]),
-]);
